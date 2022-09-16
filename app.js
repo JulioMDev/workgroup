4b0dea0610dcef39b9c5600ffac5ce38f3391d36
Furio <juliomrodriguezr@gmail.com>
2022-09-15 23:42:55 -0500

    Update: Cambios Mayores
    Agregado nuevo componente card para los productos.
    Mejorado form con validaciones y uso de atributos.
    Agregados más tipos de inputs para adicionar variedad dentro del form.
    Se mejoran estilos de los diferentes estados de los elementos.

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ function updateCart()
   let cart_count = document.getElementById('cart_count');
   cart_count.innerHTML = parseInt(cart_count.innerHTML) + 1;
 }
+
+function addToFavorite()
+{
+  iziToast.success({
+    title: 'Agregado',
+    message: 'Producto agregado a favoritos',
+  });
+}
