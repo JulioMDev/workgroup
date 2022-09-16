9fa413cda2ce9ae84ccca6ae0f86d38236403e4e
Furio <juliomrodriguezr@gmail.com>
2022-09-15 21:46:26 -0500

    Update: Estilizando Index
    Se agrega FontAwesome
    Se agrega la librería IziToast para el uso de notificaciones al momento de accionar ciertas cosas.
    Se agregan funciones de prueba en javascript para crear cierta interactividad/reactividad en el documento.

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+Agregar items a carrito de compras
+*/
+
+function addToCart(id){
+    iziToast.success({
+        title: 'Agregado',
+        message: 'Producto agregado al carrito',
+    });
+    updateCart();
+}
+
+function updateCart()
+{
+  let cart_count = document.getElementById('cart_count');
+  cart_count.innerHTML = parseInt(cart_count.innerHTML) + 1;
+}
